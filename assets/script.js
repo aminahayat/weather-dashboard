d9f906773c3a59d6e9cfcf29127567159f1a197f
Amina Hayat <hayat.amina@hotmail.com>
2022-06-05 16:11:10 +0100

    ammedned js

--- assets/script.js
+++ assets/script.js
@@ -1,120 +1,141 @@
-const APIKey = "96e6602198cf73c73eb7efc693fa0bdb";
-const testCity = "Birmingham";
-const currentWeatherElement = document.getElementById('current-weather-element');
-const forecast0 = document.getElementById('forecast-0');
-const forecast1 = document.getElementById('forecast-1');
-const forecast2 = document.getElementById('forecast-2');
-const forecast3 = document.getElementById('forecast-3');
-const forecast4 = document.getElementById('forecast-4');
+/*Global Variable Memory */
+const submitButton = document.getElementById("search-button");
+const inputBox = document.getElementById("city-input");
+const apiKey = "e005f427432a127e4dbd6f5d523c847e";
+const cityName = document.getElementById("city-name");
+const currentDate = moment().format("(L)");
+const currentTemperature = document.getElementById("temperature");
+const currentHumidity = document.getElementById("humidity");
+const currentWindSpeed = document.getElementById("wind-speed");
+const currentUV = document.getElementById("UV-index");
+const currentIcon = document.getElementById("current-icon");
+const forecast = document.querySelector(".forecast");
+const current = document.querySelector(".current")
+const historyElement = document.getElementById("history");
 
-// Retrieve latitude and longitude for city input, using the Open Weather API.
-function getOpenWeather(city) {
-  const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-  fetch(requestURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const latitude = data.coord.lat;
-      const longitude = data.coord.lon;
-      getOneCallWeather(latitude, longitude);
-    });
-}
 
-// Log all needed data from Open Call API.
-function getOneCallWeather(latitude, longitude) {
-  const requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts,minutely,hourly&appid=${APIKey}`;
-  fetch(requestURL)
+
+
+/*Function to fetch data from Current Weather API*/
+function getCurrentWeather(city) {
+
+  let currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  fetch(currentWeatherApi)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      const currentWeatherData = weatherForToday(data);
-      displayCurrentWeather(currentWeatherData);
-      const weatherForecastData = forecastWeatherData(data);
-      displayWeatherForecast(weatherForecastData);
+
+      // array for local storage 
+          
+      let nameValue = data.name;
+      cityName.innerHTML = nameValue + " " + currentDate;
+      console.log(nameValue);
+
+      let currentIconValue = data.weather[0].icon;
+      currentIcon.innerHTML = "Icon:" + currentIconValue;
+      currentIcon.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" + currentIconValue + "@2x.png"
+      );
+      console.log(currentIconValue);
+
+      let tempValue = data.main.temp;
+      let celsiusTemp = tempValue - 273.15;
+      currentTemperature.innerHTML =
+        "Temp:" + " " + Math.floor(celsiusTemp) + "°C";
+      console.log(tempValue);
+
+      let humidityValue = data.main.humidity;
+      currentHumidity.innerHTML = "Humidity:" + " " + humidityValue + "%";
+      console.log(humidityValue);
+
+      let windSpeedValue = data.wind.speed;
+      currentWindSpeed.innerHTML = "Wind:" + " " + windSpeedValue + " " + "MPH";
+      console.log(windSpeedValue);
+
+      /* Fetching data for latitude and longitude to display UV info*/
+
+      let latitude = data.coord.lat;
+      let longitude = data.coord.lon;
+      console.log(data.coord);
+      getForecast(latitude, longitude)
+      
     });
 }
 
-function weatherForToday(data) {
-  const icon = `${data.current.weather[0].icon}.png`;
-  const temperature = getTemperature(data.current.temp);
-  const humidity = `${data.current.humidity}%`;
-  const windSpeed = getWindSpeed(data.current.wind_speed);
-  const ultravioletIndex = Math.round(data.current.uvi);
-  return { icon: icon, temperature: temperature, humidity: humidity, windSpeed: windSpeed, ultravioletIndex: ultravioletIndex};
+function getForecast(latitude, longitude){
+  let oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`;
+      fetch(oneCallApi)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          displayForecast(data)
+          
+          /* Adding button is JS for UV data */
+          
+          const uvButton = document.createElement("button");
+          uvButton.classList.add("btn");
+
+          // passing the value and get the UV data 
+          uvButton.innerHTML = data.current.uvi;
+
+          // if else statement to indicate whether UV is favorable (green), moderate(yellow) or severe(red)
+          if (data.current.uvi < 3) {
+            uvButton.classList.add("btn-success");
+          }
+          else if (data.current.uvi < 7) {
+            uvButton.classList.add("btn-warning");
+          }
+          else {
+            uvButton.classList.add("btn-danger");
+          }
+
+          /* appending uvButton to currentUV div*/
+          currentUV.innerHTML = "UV Index:" + " ";
+          currentUV.appendChild(uvButton);
+
+        });
 }
 
-// Function to take raw data of exact temp in Kelvin, and return rough temp in Celsius.
-function getTemperature(kelvin) {
-  const celsius = Math.round(kelvin - 273.15);
-  return `${celsius}°C`
+
+function displayForecast(data){
+  const forecastData = 5
+  
+    for (let i = 0; i < forecastData; i++) {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+      const cardBody = document.createElement("div");
+      cardBody.setAttribute("class", "card-body");
+      const cardText = document.createElement("div");
+      cardText.setAttribute("class", "card-text");
+      const temp = document.createElement("p");
+      temp.setAttribute("class", "weather-forecast");
+      const humidity = document.createElement("p");
+      humidity.setAttribute("class", "weather-forecast");
+      const wind = document.createElement("p");
+      wind.setAttribute("class", "weather-forecast");
+      const icon = document.createElement("img");
+      icon.setAttribute("src",`https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`);
+      icon.style.width = "4rem";
+      temp.textContent = "Temperature:" + " " + data.daily[i].temp.day + "°C";
+      humidity.textContent = "Humidity:" + " " + data.daily[i].humidity + "%";
+      wind.textContent = "Humidity:" + " " + data.daily[i].wind_speed + " " + "MPH";
+      cardText.append(temp, humidity, wind)
+      cardBody.append(cardText)
+      card.append(icon, cardBody)
+      forecast.append(card) 
+  
+    }
+  
 }
 
-// Function to convert data to MPH, return to two decimal places.
-function getWindSpeed(metersPerSecond) {
-  const milesPerHour = Number.parseFloat(metersPerSecond / 0.44704).toFixed(2);
-  return `${milesPerHour}mph`;
-}
 
-// jQuery form to retrieve user input, and get weather for location given by user.
-const cityForm = $("#city-form");
-
-cityForm.on("submit", submitCity);
-
-function submitCity(event) {
-  event.preventDefault();
-  const city = $("input#city-input").val();
-  console.log(city);
-  getOpenWeather(city);
-  $("input#city-input").val("");
-}
-
-// Function to write today's weather to the page. 
-function displayCurrentWeather(collectedData) {
-  currentWeatherElement.innerHTML =
-    `<img id="today-img" src="icons/${collectedData.icon}" alt="An icon representing the weather conditions"><br/>
-    Temperature: ${collectedData.temperature}<br/>
-    Humidity: ${collectedData.humidity}<br/>
-    Wind Speed: ${collectedData.windSpeed}<br/>
-    UV Index: <span id="uv-element-${collectedData.ultravioletIndex}">${collectedData.ultravioletIndex}</span><br/>`;
-}
-
-// Function to collect forecast data for today and next 4 days
-function forecastWeatherData(data) {
-  let weatherForecastData = {
-    day0: [],
-    day1: [],
-    day2: [],
-    day3: [],
-    day4: [],
-  };
-  const objectLocations = [weatherForecastData.day0, weatherForecastData.day1, weatherForecastData.day2, weatherForecastData.day3, weatherForecastData.day4];
-
-  for (let i = 0; i < 5; i++) {
-    const unixDate = data.daily[i].dt;
-    const icon = `${data.daily[i].weather[0].icon}.png`;
-    const temp = getTemperature(data.daily[i].temp.day);
-    const humidity = `${data.daily[i].humidity}%`;
-    const windSpeed = getWindSpeed(data.daily[i].wind_speed);
-
-    const objectLocation = objectLocations[i];
-    objectLocation.push(unixDate, icon, temp, humidity, windSpeed);
-  }
-  return weatherForecastData;
-}
-
-// Function to display forecast data for today and next 4 days
-function displayWeatherForecast(weatherForecastData) {
-  const forecastElements = [forecast0, forecast1, forecast2, forecast3, forecast4];
-  const weatherData = [weatherForecastData.day0, weatherForecastData.day1, weatherForecastData.day2, weatherForecastData.day3, weatherForecastData.day4];
-  for (let i = 0; i < forecastElements.length; i++) {
-    const currentForecastElement = forecastElements[i];
-    currentForecastElement.innerHTML =
-    `<img class="forecast-img" src="icons/${weatherData[i][1]}" alt="An icon representing the weather conditions"><br>
-    Temperature: ${weatherData[i][2]}<br/>
-    Humidity: ${weatherData[i][3]}<br/>
-    Wind Speed: ${weatherData[i][4]}<br/>`;
-  };
-}
+submitButton.addEventListener("click", (event)=>{
+  event.preventDefault()
+  const city = inputBox.value;
+  getCurrentWeather(city);
+})
